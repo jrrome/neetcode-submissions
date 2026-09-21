9a44bbe8de6aa9ced50100a249722aaa7713a6f9
Jaime Romero Martínez <jromeromartinez@icloud.com>
2026-09-21 20:06:48 +0200

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        Integer[] indexes = new Integer[position.length];
+        for (int i = 0; i < position.length; i++) {
+            indexes[i] = i;
+        }
+        Arrays.sort(indexes, Comparator.comparingInt(i -> position[i]));
+
+        ArrayDeque<Float> stack = new ArrayDeque<>();
+
+        if (indexes.length > 0) {
+            int index = indexes[0];
+            stack.addLast((float) (target - position[index]) / speed[index]);
+        }
+        for (int i = 0; i < indexes.length; i++) {
+            int index = indexes[i];
+            float time = (float) (target - position[index]) / speed[index];
+            while (!stack.isEmpty() && time >= stack.getLast()) {
+                stack.removeLast();
+            } 
+            stack.addLast(time);
+        }
+        return stack.size();
+    }
+}
